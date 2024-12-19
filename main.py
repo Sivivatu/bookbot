52f6c54471b07ef92aea9a4db12162119c38d4ee
Paul <Sivivatu@users.noreply.github.com>
2024-12-19 13:52:58 +0000

    add report code

---
+++ main.py
@@ -0,0 +1,39 @@
+letters = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')
+
+def unique_characters(string) -> dict:
+    chars = {}
+
+    for character in string:
+        if character in letters:    
+            lower = character.lower()       
+            if lower in chars:
+                chars[lower] += 1
+            else:
+                chars[character] = 1
+    return chars
+
+
+def count_words(text: str):
+    words = text.split()
+    return len(words)
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+def main():
+    path_to_file = "books/frankenstein.txt"
+    book_text = get_book_text(path_to_file)
+    book_count = count_words(book_text)
+    character_count = unique_characters(book_text)
+
+    print(f"--- Begin report of {path_to_file} ---")
+    print(f"{book_count} words found in the document")
+    print("\n")
+    for c in sorted(character_count, key=character_count.get, reverse=True):
+        print(f"The character '{c}' was found {character_count[c]} times")
+    print("--- End report ---")
+
+
+if __name__ == "__main__":
+    main() 
