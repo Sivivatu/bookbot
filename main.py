1d988218808ae68782c68b02c73b9787a22e6032
Paul Houghton <paul.j.houghton@gmail.com>
2025-03-01 19:33:38 +0000

    working calculation at end of Ch2

--- main.py
+++ main.py
@@ -1,47 +1,4 @@
-from stats import get_num_words, get_book_text
-
-
-letters = (
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-)
-
-
-def unique_characters(string) -> dict:
-    chars = {}
-
-    for character in string:
-        if character in letters:
-            lower = character.lower()
-            if lower in chars:
-                chars[lower] += 1
-            else:
-                chars[character] = 1
-    return chars
+from stats import get_num_words, get_book_text, unique_characters
 
 
 def main():
