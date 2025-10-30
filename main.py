e8f8f3013c038c96907a6b5aa1a764216a3f3620
Paul Houghton <paul.j.houghton@gmail.com>
2025-10-30 15:59:17 +0000

    update usage hint to 'uv run', use explicit lambda for sorting character counts, add blank line

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from stats import get_num_words, get_book_text, unique_characters
 
 inputs = sys.argv
 
+
 def main():
     if len(sys.argv) != 2:
-        print("Usage: python3 main.py <path_to_book>")
+        print("Usage: uv run main.py <path_to_book>")
         return sys.exit(1)
     path_to_file = sys.argv[1]
     book_text = get_book_text(path_to_file)
@@ -17,7 +18,7 @@ def main():
     print("----------- Word Count ----------")
     print(f"Found {book_count} total words")
     print("--------- Character Count -------")
-    for c in sorted(character_count, key=character_count.get, reverse=True):
+    for c in sorted(character_count, key=lambda x: character_count[x], reverse=True):
         print(f"{c}: {character_count[c]}")
     print("============= END ===============")
 
